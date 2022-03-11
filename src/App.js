4c9a7780f5556a94de43dd46b6826cb2e3c4d389
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-03-10 19:55:08 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+    // Api key needed.
+
+    const API_URL = 'http://ww.amdbapi.com?apikey=';
+    return (
+        <h1>App</h1>
+    );
+}
+
